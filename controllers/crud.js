6fd80800d4716c6f8f113b339c5ab36995ae4d64
josedubrez <jldubrez@gmail.com>
2022-02-10 18:58:47 -0300

    Se agrega CRUD para servicios

--- controllers/crud.js
+++ controllers/crud.js
@@ -2,27 +2,54 @@
 const pool = require('../database/db');
 
 //GUARDAR un REGISTRO
-exports.save = async (req, res)=>{
+exports.save_error = async (req, res)=>{
     const codigo = req.body.codigo;
     const descripcion = req.body.descripcion;
     let rs
     try{
         rs = await pool.query(`INSERT INTO errores (codigo, descripcion) VALUES ('${codigo}' , '${descripcion}')`)
-        res.redirect('/');
+        res.redirect('/errores');
     }catch(e){
         console.log(e)
     }
 };
 
 //ACTUALIZAR un REGISTRO
-exports.update = async (req, res) => {
+exports.update_error = async (req, res) => {
     const id = req.body.id;
     const codigo = req.body.codigo;
     const descripcion = req.body.descripcion;
     let rs
     try{
         rs = await pool.query(`UPDATE errores SET codigo = '${codigo}', descripcion = '${descripcion}' WHERE id = ${id}`)
-        res.redirect('/');
+        res.redirect('/errores');
+    }catch(e){
+        console.log(e)
+    }
+};
+
+//GUARDAR un REGISTRO
+exports.save_servicio = async (req, res)=>{
+    const especificaciones = req.body.especificaciones;
+    const descripcion = req.body.descripcion;
+    let rs
+    try{
+        rs = await pool.query(`INSERT INTO servicios (descripcion, especificaciones) VALUES ('${descripcion}' , '${especificaciones}')`)
+        res.redirect('/servicios');
+    }catch(e){
+        console.log(e)
+    }
+};
+
+//ACTUALIZAR un REGISTRO
+exports.update_servicio = async (req, res) => {
+    const id = req.body.id;
+    const especificaciones = req.body.especificaciones;
+    const descripcion = req.body.descripcion;
+    let rs
+    try{
+        rs = await pool.query(`UPDATE servicios SET descripcion = '${descripcion}', especificaciones = '${especificaciones}' WHERE id_servicios = ${id}`)
+        res.redirect('/servicios');
     }catch(e){
         console.log(e)
     }
--- router.js
+++ router.js
@@ -7,18 +7,40 @@ const pool = require('./database/db');
 router.get('/', async (req, res) => {     
     let rs 
     try{
-        rs = await pool.query('SELECT * FROM errores ORDER BY 1')
-        res.render('index.ejs', {results:rs.rows});
+        rs = await pool.query('SELECT * FROM servicios ORDER BY descripcion')
+        res.render('index_servicios', {results:rs.rows});
     }catch(e){
         console.log(e)
     }
 })
 
-router.get('/create', (req,res)=>{
+router.get('/servicios', async (req, res) => {     
+    let rs 
+    try{
+        rs = await pool.query('SELECT * FROM servicios ORDER BY descripcion')
+        res.render('index_servicios', {results:rs.rows});
+    }catch(e){
+        console.log(e)
+    }
+})
+
+router.get('/errores', async (req, res) => {     
+    let rs 
+    try{
+        rs = await pool.query('SELECT * FROM errores ORDER BY 1')
+        res.render('index', {results:rs.rows});
+    }catch(e){
+        console.log(e)
+    }
+})
+
+
+
+router.get('/errores/create', (req,res)=>{
     res.render('create');
 })
 
-router.get('/edit/:id', async (req,res)=>{    
+router.get('/errores/edit/:id', async (req,res)=>{    
     const id = req.params.id;
     let rs
     try{
@@ -29,18 +51,47 @@ router.get('/edit/:id', async (req,res)=>{
     }
 });
 
-router.get('/delete/:id', async (req, res) => {
+router.get('/errores/delete/:id', async (req, res) => {
     const id = req.params.id;
     let rs
     try{
         rs = await pool.query(`DELETE FROM errores WHERE id = ${id}`)
-        res.redirect('/');
+        res.redirect('/errores');
     }catch(e){
         console.log(e)
     }
 });
 
-router.post('/save', crud.save);
-router.post('/update', crud.update);
+
+router.get('/servicios/create', (req,res)=>{
+    res.render('create_servicios');
+})
+
+router.get('/servicios/edit/:id', async (req,res)=>{    
+    const id = req.params.id;
+    let rs
+    try{
+        rs = await pool.query(`SELECT * FROM servicios WHERE id_servicios = ${id}`)
+        res.render('edit_servicios', {servicio: rs.rows[0]});
+    }catch(e){
+        console.log(e)
+    }
+});
+
+router.get('/servicios/delete/:id', async (req, res) => {
+    const id = req.params.id;
+    let rs
+    try{
+        rs = await pool.query(`DELETE FROM servicios WHERE id_servicios = ${id}`)
+        res.redirect('/servicios');
+    }catch(e){
+        console.log(e)
+    }
+});
+
+router.post('/errores/save', crud.save_error);
+router.post('/errores/update', crud.update_error);
+router.post('/servicios/save', crud.save_servicio);
+router.post('/servicios/update', crud.update_servicio);
 
 module.exports = router;
